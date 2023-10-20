c5ffc44456ae4efbe07016ccbc167b6722c7547c
beni-f <benifissha@gmail.com>
2023-10-20 18:23:12 +0300

    updated

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -26,10 +26,24 @@ void bubble_sort(int *array, size_t size)
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
 				flag = 1;
+				print_array(array, size);
 			}
-			print_array(array, size);
 		}
 		if (flag == 0)
 			break;
 	}
+}
+
+
+int main(void)
+{
+    int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
+    size_t n = sizeof(array) / sizeof(array[0]);
+
+    print_array(array, n);
+    printf("\n");
+    bubble_sort(array, n);
+    printf("\n");
+    print_array(array, n);
+    return (0);
 }
